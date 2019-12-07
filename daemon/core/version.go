4af82c532b1388ea106e0ed0ab762f0ad95cd911
Noel Kuntze <noel@familie-kuntze.de>
2019-12-07 15:19:45 +0100

    0.4

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.0.0b"
+	Version = "0.4"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
